153cf710bec9810a0d1170b059177564628b4053
ShwetaBodake <shwetabodake8108@gmail.com>
2022-12-24 21:08:16 +0530

    Refactor the Employee Wage Computation Program

---
+++ UC7_RefactorEmployeeWageComputation.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace EmpolyeeWageComputation_Day9
+{
+	public class UC7_RefactorEmployeeWageComputation
+	{
+        public const int IS_PART_TIME = 2;
+        public const int IS_FULL_TIME = 1;
+        public const int EMP_Wage_PER_HOUR = 20;
+        public const int TotalDaysInMonth = 20;
+        public const int MAX_Hrs_In_Month = 100;
+
+        public void RefactorEmployeeWageComputation()
+		{
+            int emphr = 0;
+            int totalempHrs = 0;
+            int totalworkingdays = 0;
+
+            while (totalempHrs <= MAX_Hrs_In_Month && totalworkingdays < TotalDaysInMonth)
+            {
+                totalworkingdays++;
+
+                Random random = new Random();
+                int CheckEmp = random.Next(0, 3);
+
+                switch (CheckEmp)
+                {
+                    case IS_FULL_TIME:
+                        emphr = 8;
+                        break;
+
+                    case IS_PART_TIME:
+                        emphr = 4;
+                        break;
+                    default:
+                        emphr = 0;
+                        break;
+                }
+                totalempHrs += emphr;
+                Console.WriteLine("Day" + totalworkingdays + "emphrs" + emphr);
+            }
+            int totalempWage = totalempHrs * EMP_Wage_PER_HOUR;
+            Console.WriteLine("Total Wage of employee" + totalempWage);
+        }
+	}
+}
+
