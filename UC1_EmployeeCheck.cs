01ba389e4e6deea23eaf88cc2d3b81ebb269a8be
ShwetaBodake <shwetabodake8108@gmail.com>
2022-12-24 18:53:31 +0530

    Solution of Employee check

---
+++ UC1_EmployeeCheck.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace EmpolyeeWageComputation_Day9
+{
+	public class UC1_EmployeeCheck
+	{
+		public void EmployeeCheck()
+		{
+			int IS_FULL_TIME = 1;
+
+			Random random = new Random();
+			int EMP_Check = random.Next(0, 2);
+
+			if (EMP_Check == 1)
+			{
+				Console.WriteLine("Employee is Present");
+			}
+			else
+				Console.WriteLine("Employee is Absent.");
+		}
+	}
+}
+
