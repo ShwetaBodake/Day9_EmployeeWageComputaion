debb5ac7d4f812693ceb0060ddbe0a03206222c4
ShwetaBodake <shwetabodake8108@gmail.com>
2022-12-24 18:51:20 +0530

    Welcome Message

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using EmpolyeeWageComputation_Day9;
+
+namespace EmployeeWageComputation_Day9
+{
+public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("-------WelCome To Employee Wage Computation------------");
+
+            UC1_EmployeeCheck UC1_EmployeeCheck = new UC1_EmployeeCheck();
+            UC1_EmployeeCheck.EmployeeCheck();
+        }
+    }
+}
