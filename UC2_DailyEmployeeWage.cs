bed0a07b0fa37444650b6444cd0632b63dbcce7e
ShwetaBodake <shwetabodake8108@gmail.com>
2022-12-24 19:03:00 +0530

    Calculate Daily Employee Wage

---
+++ UC2_DailyEmployeeWage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace EmpolyeeWageComputation_Day9
+{
+	public class UC2_DailyEmployeeWage
+	{
+		public void DailyEmployeeWage()
+		{
+
+			//Constants
+			int IS_FULL_TIME = 1;
+			int EMP_WAGE_PER_HOUR = 20;
+
+			//Variables
+			int empHr = 0;
+			int empWage = 0;
+
+			Random random = new Random();
+			int CheckEmp = random.Next(0, 2);
+
+			if (CheckEmp == 1)
+			{ Console.WriteLine("Employee is PRESENT");
+				empHr = 8; }
+			else
+			{ Console.WriteLine("Employee is Absent"); }
+
+			empWage = empHr * EMP_WAGE_PER_HOUR;
+			Console.WriteLine("Daily Employee Wage is :-" + empWage);
+
+
+
+        }
+	}
+}
+
